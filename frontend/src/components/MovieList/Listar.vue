e4e30d20f220ff5f4de555743b41a6c5e8c1ec11
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-11-30 16:38:22 -0300

    return data pelis api with console

--- frontend/src/components/MovieList/Listar.vue
+++ frontend/src/components/MovieList/Listar.vue
@@ -5,11 +5,16 @@
 <script>
 export default {
     created:function(){
-
+        this.consultarPelis();
     },
     methods:{
         consultarPelis(){
-            fetch('http://localhost:3000/peliculas');
+            fetch('http://localhost:3000/peliculas')
+            .then(res=>res.json())
+            .then((datosRespuesta)=>{
+                console.log(datosRespuesta)
+            })
+            .catch(console.log);
         }
     }
 }
