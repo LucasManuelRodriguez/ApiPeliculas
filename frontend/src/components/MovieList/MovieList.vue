3d8c73a352fde65a5d33ee1f3f75790a1ee56f2a
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-11-29 22:47:00 -0300

    add function edit & update

--- frontend/src/components/MovieList/MovieList.vue
+++ frontend/src/components/MovieList/MovieList.vue
@@ -2,11 +2,16 @@
   <div>
       <h1>Lista de Pelis</h1>
       <ul>
-          <li v-for="(movie, index) in movies" :key='index'>{{movie.title}}<button @click="removeMovie(index)">Delete</button></li>
+          <li v-for="(movie, index) in movies" :key="index">
+              {{index}}-{{movie.title}}
+              <button @click="removeMovie(index)">Delete</button>
+              <button @click="editMovie(index)">Edit</button>
+          </li>
       </ul>
-      <input type="text" v-model="newMovie.title">
-      <button @click="addMovie">Add</button>
-      <p>{{newMovie.title}}</p>
+      <input type="text" v-model="newMovie.title" />
+      <button v-if="mode =='add'" @click="addMovie">Add</button>
+      <button v-if="mode == 'edit'" @click="updateMovie">Update</button>
+      <p>{{newMovie.index}}-{{newMovie.title}}</p>
   </div>
 </template>
 
@@ -14,6 +19,8 @@
 export default {
     data(){
         return{
+            indexToUpdate:"",
+            mode:"add",
             newMovie:{},
             movies:[{title:'Batman'},{title:'Sherk'}],
         };
@@ -26,6 +33,16 @@ export default {
         },
         removeMovie(index){
             this.movies.splice(index,1);
+        },
+        editMovie(index){
+            this.mode='edit';
+            this.newMovie = this.movies[index];
+            this.newMovie.index = index;
+        },
+        updateMovie(){
+            this.movies[this.indexToUpdate]= this.newMovie;
+            this.newMovie={};
+            this.mode='add';
         }
     },
 }
