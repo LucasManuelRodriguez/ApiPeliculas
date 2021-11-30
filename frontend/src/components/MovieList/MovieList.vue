bf926873d59dd91195601a54b9e035177d13a3ad
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-11-29 21:06:19 -0300

    add array movies

--- frontend/src/components/MovieList/MovieList.vue
+++ frontend/src/components/MovieList/MovieList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-      <h1>{{msg}}</h1>
+      <h1>Lista de Pelis</h1>
+      <ul>
+          <li v-for="(movie, index) in movies" :key='index'>{{movie.title}}</li>
+      </ul>
+      <input type="text" v-model="newMovie">
+      <p>{{newMovie}}</p>
   </div>
 </template>
 
@@ -8,12 +13,15 @@
 export default {
     data(){
         return{
-            msg:'Hola'
+            newMovie:'',
+            movies:[{title:'Batman'},{title:'Sherk'}],
         }
     }
 }
 </script>
 
 <style>
-
+ul{
+    list-style: none;
+}
 </style>
