b16afeeb3ed4fba6106a94f7c6aae9da1fd98e19
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-11-30 16:55:25 -0300

    function fetch ok

--- frontend/src/components/MovieList/Listar.vue
+++ frontend/src/components/MovieList/Listar.vue
@@ -1,9 +1,22 @@
 <template>
-  
+    <div>
+        <div v-for="pelicula in peliculas" :key="pelicula.id" class="card" style="width: 18rem;">
+            <h2>{{pelicula.nombre}}</h2>
+            <div class="card-body">
+                <h5>{{pelicula.director}}</h5>
+                <p class="card-text">{{pelicula.clasificacion}}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            peliculas:[],
+        }
+    },
     created:function(){
         this.consultarPelis();
     },
@@ -13,6 +26,10 @@ export default {
             .then(res=>res.json())
             .then((datosRespuesta)=>{
                 console.log(datosRespuesta)
+                this.peliculas=[]
+                if(typeof datosRespuesta[0].success==='undefined'){
+                    this.peliculas=datosRespuesta;
+                }
             })
             .catch(console.log);
         }
